d545515fc956fab80fcbe8e2ad0008c9e98b8d7f
19.kms.70 <19.kms.70@gmail.com>
2022-07-29 18:13:51 +0300

    initial files added

---
+++ conftest.py
@@ -0,0 +1,49 @@
+﻿import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help="Choose browser: chrome or firefox")
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language: ru or en-gb (for example)")
+
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    print(f"{language=}")
+    if not language:
+        raise pytest.UsageError("--language should be defined ( ru,en for example )")
+    else:
+        print(f"\nstart chrome browser with {language} language")
+
+
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        from selenium.webdriver.chrome.options import Options
+        options = Options()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+
+
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        from selenium.webdriver.firefox.options import Options
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        options = Options()
+        options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+        browser = webdriver.Firefox(options=options,firefox_profile=fp)
+
+
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
