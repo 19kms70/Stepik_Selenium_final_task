38398f86a7c1259ff72c302cb9c1da765ae9daca
19.kms.70 <19.kms.70@gmail.com>
2022-07-30 18:00:51 +0300

    4.3.3-data_independence

--- test_product_page.py
+++ test_product_page.py
@@ -4,7 +4,8 @@ from selenium.webdriver.common.by import By
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage
 
-LINK="https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+LINK = "https://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+LINK = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
 
 def test_guest_can_add_product_to_basket(browser):
     link = LINK
